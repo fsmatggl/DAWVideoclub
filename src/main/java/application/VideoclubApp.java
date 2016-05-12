dbfa66077bcd9561eff68220f56a29c83d3dac79
David <dso_27@hotmail.com>
2016-05-12 13:25:03 +0200

    Elimino comentario

--- src/main/java/application/VideoclubApp.java
+++ src/main/java/application/VideoclubApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VideoclubApp {
-	//Esta clase inicia toda la aplicacion web
 	public static void main(String[] args) {
 		SpringApplication.run(VideoclubApp.class, args);
 	}
