97ddcbb0dee59515af9d26c5cf35617f1458257a
Daniel Funke Prieto <d.funke@alumnos.upm.es>
2016-05-20 15:08:31 +0100

    User management page and access through home

--- src/main/java/daw/videoclub/controller/ApplicationController.java
+++ src/main/java/daw/videoclub/controller/ApplicationController.java
@@ -1,16 +1,30 @@
 package daw.videoclub.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import daw.videoclub.model.User;
+import daw.videoclub.repository.UserRepository;
+
 @Controller
 public class ApplicationController {
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	@RequestMapping("/")
-	public ModelAndView home(){
-		return new ModelAndView("home");
+	public ModelAndView home(Principal principal){
+		User u = userRepository.findByUsername(principal.getName());
+		List<GrantedAuthority> roles = u.getRoles();
+		boolean admin = roles.contains( new SimpleGrantedAuthority("ROLE_ADMIN"));
+		return new ModelAndView("home").addObject("admin", admin);
 	}
 }
